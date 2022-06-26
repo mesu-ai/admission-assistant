b9bdb89754ae002edccc5584ddd1ed8f8242ec13
mesu-ai <mesu.s.ss@gmail.com>
2022-06-26 12:10:09 +0600

    redux-setup

---
+++ src/app/features/addmission/addmissionSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const admissionSlice=createSlice({
+    name:'admission',
+    initialState:{
+
+    },
+    extraReducers:{
+
+
+    }
+});
+
+export default admissionSlice.reducer;
